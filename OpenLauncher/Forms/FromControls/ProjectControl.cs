5ba9c06161a41bc089fe3487bd996e9b6253469e
XanatosX <simonaberle@gmx.de>
2018-04-06 10:34:44 +0200

    Added comment

--- OpenLauncher/Forms/FromControls/ProjectControl.cs
+++ OpenLauncher/Forms/FromControls/ProjectControl.cs
@@ -57,8 +57,8 @@ namespace OpenLauncher.Forms.FromControls
             init();
             if (_data.WebURL != null)
             {
-                WB_ProjectMainPage.Navigate(_data.WebURL);
-                //WB_ProjectMainPage.Url = _data.WebURL;
+                //@NOTE find a way to speed up the page loading. Maybe if the web browser is async
+                WB_ProjectMainPage.Url = _data.WebURL;
             }
             else
             {
